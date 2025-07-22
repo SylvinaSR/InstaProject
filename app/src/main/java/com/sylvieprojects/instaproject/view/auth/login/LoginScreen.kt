26bd18e1f57ba8bf5d71e9d9d0125262486d08f7
Sylvie Salazar <sylvina_srosales@hotmail.com>
2025-07-22 17:17:12 -0600

    fix: add dependency injection in viewModel

--- app/src/main/java/com/sylvieprojects/instaproject/view/auth/login/LoginScreen.kt
+++ app/src/main/java/com/sylvieprojects/instaproject/view/auth/login/LoginScreen.kt
@@ -19,8 +19,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sylvieprojects.instaproject.R
 import com.sylvieprojects.instaproject.view.core.components.InstaButton
 import com.sylvieprojects.instaproject.view.core.components.InstaIcon
@@ -30,7 +30,7 @@ import com.sylvieprojects.instaproject.view.core.components.InstaText
 
 @Composable
 fun LoginScreen(
-    loginViewModel: LoginViewModel = viewModel(),
+    loginViewModel: LoginViewModel = hiltViewModel(),
     navigateToLogin: () -> Unit,
     navigateToSignUp: () -> Unit
 ) {
--- app/src/main/java/com/sylvieprojects/instaproject/view/auth/login/LoginViewModel.kt
+++ app/src/main/java/com/sylvieprojects/instaproject/view/auth/login/LoginViewModel.kt
@@ -2,11 +2,14 @@ package com.sylvieprojects.instaproject.view.auth.login
 
 import android.util.Patterns
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
-class LoginViewModel : ViewModel() {
+@HiltViewModel
+class LoginViewModel @Inject constructor(): ViewModel() {
 
     private val _uiState = MutableStateFlow(LoginUiState())
     val uiState: StateFlow<LoginUiState> = _uiState
